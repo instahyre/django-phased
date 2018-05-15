952c0127d87d02bda41cb9061ff5affaf77ad077
Pritish Chakraborty <chakrabortypritish@gmail.com>
2018-05-15 14:19:00 +0530

    Fix to deal with breaking changes from Django v1.6

--- phased/models.py
+++ phased/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.utils.hashcompat import sha_constructor
+from hashlib import sha1 as sha_constructor
 
 if not hasattr(settings, 'PHASED_SECRET_DELIMITER'):
     settings.PHASED_SECRET_DELIMITER = sha_constructor(getattr(settings, 'SECRET_KEY', '')).hexdigest()
